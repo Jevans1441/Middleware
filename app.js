21ac2d055a2bce1e4f5a043b0c9a3f464ae7ad23
Josh Evans <88986229+Jevans1441@users.noreply.github.com>
2021-12-21 22:01:05 -0500

    ejs working

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 const http = require("http");
 const express = require("express");
-
+const morgan = require("morgan");
+const logger = morgan("tiny");
 const app = express();
 const server = http.createServer(app);
+const db = require("./db");
+const { name } = require("ejs");
 
 const HOST = "127.0.0.1";
 const PORT = 3001;
 
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path}`);
-  next();
-});
+app.use(logger);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("Hi");
+  res.render("partials/home");
   console.log("home page");
+});
+
+app.get("/ceos", (req, res) => {
+  res.render("partials/ceo-list", {
+    data: db,
+  });
+});
+
+app.get("/:slug", (req, res) => {
+  let link = req.params.slug;
+  const ceo = db.find((ceo) => ceo.slug === link);
+  res.render("partials/ceo-details", { name: ceo.name, year: ceo.year });
 });
 
 server.listen(PORT, HOST, () => {
